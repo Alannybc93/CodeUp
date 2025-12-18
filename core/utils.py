bfc7ce2b723ff19c728f338dd3e3083455bfd5dc
Alanny Barbosa Cavalcante <alanny.cavalcante@aluno.ifsertao-pe.edu.br>
2025-12-18 18:43:38 -0300

    feat: adicionar utilitários gerais do sistema
    - gerar_codigo_convite() para convites únicos
    - calcular_tempo_estudo() para sessões
    - formatar_tempo() para exibição amigável
    - Funções reutilizáveis para todo o sistema

---
+++ core/utils.py
@@ -0,0 +1,25 @@
+﻿# utils.py - Utilitários para o sistema CodeUp
+
+import random
+from datetime import datetime, timedelta
+
+def gerar_codigo_convite():
+    """Gera código de convite aleatório"""
+    caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    return ''.join(random.choice(caracteres) for _ in range(8))
+
+def calcular_tempo_estudo(tempo_inicio, tempo_fim):
+    """Calcula tempo total de estudo em minutos"""
+    diferenca = tempo_fim - tempo_inicio
+    return int(diferenca.total_seconds() / 60)
+
+def formatar_tempo(minutos):
+    """Formata minutos para formato legível"""
+    if minutos < 60:
+        return f"{minutos}min"
+    horas = minutos // 60
+    mins = minutos % 60
+    if mins == 0:
+        return f"{horas}h"
+    return f"{horas}h{mins}min"
+
